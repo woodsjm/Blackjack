f4de8a11a5b5773d0f4b591583e70a4536621e56
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-07-08 16:17:24 -0500

    Dealer and Player initiated

--- js/app.js
+++ js/app.js
@@ -1,12 +1,117 @@
-console.log("Showing Cards is Working");
+console.log("Blackjack Started");
+
+
+class Player {
+	constructor() {
+		this.name = name;
+		this.cardsPlayed = [];
+		this.hand = [];
+	}
+
+	// Calculate value of cards in hand, return value, and empty cards in hand
+	/*playCard() {
+		for (let i = 0; i < this.hand.length; i++) {
+
+		}
+	}*/
+}
+
+class Computer extends Player {
+
+	// Calculate value of cards in hand, return value, and empty cards in hand
+	/*playCard() {
+		const 
+	}*/
+}
 
 
 
 const game = {
-	cards: [{}],
-	cardsInHand: 
+	cards: [{
+		name: "Queen of Hearts",
+		value: 10
+	}, {
+		name: "2 of Diamonds",
+		value: 2
+	}, {
+		name: "Ace of Diamonds",
+		value: 1
+	}, {
+		name: "Jack of Diamonds",
+		value: 10
+	}, {
+		name: "King of Diamonds",
+		value: 10
+	}, {
+		name: "3 of Diamonds",
+		value: 3
+	}, {
+		name: "4 of Diamonds",
+		value: 4
+	}, {
+		name: "5 of Diamonds",
+		value: 5
+	}, {
+		name: "6 of Diamonds",
+		value: 6
+	}, {
+		name: "7 of Diamonds",
+		value: 7
+	}, {
+		name: "8 of Diamonds",
+		value: 8
+	}, {
+		name: "9 of Diamonds",
+		value: 9
+	}, {
+		name: "10 of Diamonds",
+		value: 10
+	}
+	],
 
+	computerScore: 0, 
+	playerScore: 0,
+
+	computer: null,
+	player: null,
+
+	createPlayer() {
+		const newPlayer = new Player();
+		this.player = newPlayer;
+		console.log(this.player);
+	},
+	createComputer() {
+		const newDealer = new Computer();
+		this.computer = newDealer;
+		console.log(this.computer);
+
+	},
+
+	dealCards() {
+		for (let i = 0; i < 2; i++) {
+			const cardIndexPlayer = Math.floor(Math.random() * this.cards.length);
+			this.player.hand.push(this.removeCard(cardIndexPlayer));
+
+			const cardIndexComputer = Math.floor(Math.random() * this.cards.length);
+			this.computer.hand.push(this.removeCard(CardIndexComputer));
+		}
+	},
+
+
+	removeCard(cardIndex) {
+		const cardArray = this.cards.splice(cardIndex, 1);
+		return cardArray[0]
+	}
 }
+
+
+
+game.createPlayer();
+game.createComputer();
+game.dealCards();
+console.log(game.player.hand);
+console.log(game.computer.hand);
+
 
 
 
