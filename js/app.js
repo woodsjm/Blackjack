ff3d3b099bdde8e71a9a593ce3c14a106b21dd62
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-07-08 14:47:26 -0500

    Added User Stories and Wireframes

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("Showing Cards is Working");
+
+
+
+const game = {
+	cards: [{}],
+	cardsInHand: 
+
+}
+
+
+
+	//Store cards
+
+
+
+
+	//Show cards
+
+
+
+
+	//Play cards against each other (Player vs Computer)
+
+
+
+
+	//Remove all cards from hand
